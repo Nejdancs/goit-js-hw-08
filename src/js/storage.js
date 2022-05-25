f15d50c72305eadd0f3e3b04595610a8f5a48d11
Sergey Korobka <nejdancs@gmail.com>
2022-05-25 22:49:33 +0300

    add modul storage.js

---
+++ src/js/storage.js
@@ -0,0 +1,24 @@
+export default { saveLocaleStorage, getLocalStorage };
+
+function saveLocaleStorage(key, value) {
+  try {
+    const valueToJson = JSON.stringify(value);
+    localStorage.setItem(key, valueToJson);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+function getLocalStorage(key) {
+  const savedData = localStorage.getItem(key);
+
+  if (!savedData) {
+    return;
+  }
+
+  try {
+    return JSON.parse(savedData);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
